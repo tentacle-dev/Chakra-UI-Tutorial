35b661be0b76165be0361c0b4606329bad7008f9
Thanush-dev <tanush0525@gmail.com>
2023-02-24 21:48:38 +0530

    Styled Components and Pseudo CSS - Hover[imp] with Style props

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Box, Container, Heading, Text } from "@chakra-ui/layout";
 
-function Dashboard() {
-  return (
-    <div>
-      Dashboard
-    </div>
+export default function Dashboard() {
+  const boxStyles = {
+    p: "10px",
+    bgColor:"purple.400",
+    m:"10px",
+    textAlign:"Center",
+    filter:'blur(1px)',
+    ':hover' : {
+      color : 'black',
+      fontWeight:'900',
+      bg:'blue.200'
+
+    }
+    
+  }
+  return(
+    <Container as="h1" maxWidth="4x1">
+      <Heading m={2}>
+        Chakra
+      </Heading>
+      <Text color={"blueviolet.50"} fontWeight={500}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, molestiae!</Text>
+      <Box>
+        <Text>Hello</Text>
+      </Box>
+
+      <Box sx={boxStyles}>
+        Hello, Ninjas
+
+      </Box>
+    </Container>
   )
 }
-
-export default Dashboard
