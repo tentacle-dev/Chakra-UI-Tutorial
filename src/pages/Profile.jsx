53336516ae79fa60dd00b0fb3ae487c963aa77d5
Thanush-dev <tanush0525@gmail.com>
2023-02-26 15:23:16 +0530

    Tabs

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,27 +1,57 @@
-import { TabList, TabPanel, Tabs, Tab, TabPanels } from "@chakra-ui/react";
+import { EmailIcon,ChatIcon,StarIcon, CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
+import { TabList, TabPanel, Tabs, Tab, TabPanels ,List, ListItem,ListIcon} from "@chakra-ui/react";
 
 export default function Profile() {
   return (
-    <Tabs>
+    <Tabs mt='40px' p='20px'colorScheme='purple'>
       <TabList>
-        <Tab>Name</Tab>
-        <Tab>Address</Tab>
-        <Tab>Phone Number</Tab>
-        <Tab>Gender</Tab>
-
+        <Tab _selected={{color:'white' , bg:'purple.300'}}>Account Info</Tab>
+        <Tab _selected={{color:'white' , bg:'purple.400'}}>Task History</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
-          Thanush
+        <List spacing={4}>
+            <ListItem>
+              <ListIcon as={EmailIcon} />
+              Email: mario@netninja.dev
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon} />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={StarIcon} />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+          </List>
         </TabPanel>
         <TabPanel>
-          291/6,2/1,Edward Avenue
-        </TabPanel>
-        <TabPanel>
-          777032505
-        </TabPanel>
-        <TabPanel>
-          Male
+        <List spacing={4}>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+          </List>
         </TabPanel>
       </TabPanels>
     </Tabs>
